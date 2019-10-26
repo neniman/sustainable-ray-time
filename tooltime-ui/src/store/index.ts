5ed092c8ade1e161d9bf8799db44a5bb0ca1b15f
neni <neniverena@gmx.de>
2019-10-26 11:00:05 +0200

    Initial frontend setup

---
+++ tooltime-ui/src/store/index.ts
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+enum ApplicationState {
+  LOGGED_IN_NORMAL = 'normal',
+  LOGEED_IN_SUPER = 'super', 
+  LOGGED_OUT = 'logout',
+  RETURN_TOOL = 'return'
+}
+
+export default new Vuex.Store({
+  state: {
+    state: 'logout' as ApplicationState
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
