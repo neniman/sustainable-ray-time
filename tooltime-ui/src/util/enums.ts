4966d7789c516001e04d77dd28603ceaacfe85f2
neni <neniverena@gmx.de>
2019-10-27 12:55:32 +0100

    fix

--- tooltime-ui/src/util/enums.ts
+++ tooltime-ui/src/util/enums.ts
@@ -1,6 +1,7 @@
 export enum ApplicationState {
     LOGGED_IN_NORMAL = 'normal',
     LOGGED_IN_SUPER = 'super', 
+    LOGGED_IN = 'normal',
     LOGGED_OUT = 'logout',
     RETURN_TOOL = 'return'
 }
